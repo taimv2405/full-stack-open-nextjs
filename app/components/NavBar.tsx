5881d0a5dd0fe9b966bcf23d52583044bae9aa43
taimv2405 <taimv2405@gmail.com>
2026-07-14 05:47:15 +0000

    [17] style: style navigation bar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -3,6 +3,7 @@
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useSession, signOut } from 'next-auth/react';
+import NavLink from './NavLink';
 
 const NavBar = () => {
   const { data: session } = useSession();
@@ -14,27 +15,37 @@ const NavBar = () => {
   };
 
   return (
-    <nav>
-      <Link href="/">home</Link>
+    <nav className="bg-gray-800 text-white px-6 h-14 flex items-center gap-4">
+      <NavLink href="/">home</NavLink>
       {' | '}
-      <Link href="/blogs">blogs</Link>
+      <NavLink href="/blogs">blogs</NavLink>
       {' | '}
-      <Link href="/users">users</Link>
-      {' | '}
-      {session ? (
+      <NavLink href="/users">users</NavLink>
+      {session && (
         <>
-          <Link href="/blogs/new">create new</Link>
-          {' | '}
-          <em>{session.user?.name} logged in</em>{' '}
-          <button onClick={handleLogout}>logout</button>
-        </>
-      ) : (
-        <>
-          <Link href="/login">login</Link>
-          {' | '}
-          <Link href="/register">register</Link>
+          {' | '} <NavLink href="/blogs/new">create blog</NavLink>
         </>
       )}
+
+      <div className="ml-auto flex items-center gap-4">
+        {session ? (
+          <>
+            <em className="text-gray-300">{session.user?.name} logged in</em>
+            <button
+              onClick={handleLogout}
+              className="bg-gray-600 hover:bg-gray-500 px-3 py-1 rounded text-sm"
+            >
+              logout
+            </button>
+          </>
+        ) : (
+          <>
+            <NavLink href="/login">login</NavLink>
+            {' | '}
+            <NavLink href="/register">register</NavLink>
+          </>
+        )}
+      </div>
     </nav>
   );
 };
