f5091fc021c531b6fbb5bd6f3aba5975d4b4149e
taimv2405 <taimv2405@gmail.com>
2026-07-15 09:32:59 +0000

    [23] test: drop redirect guard on /blogs/new page

--- app/blogs/new/page.tsx
+++ app/blogs/new/page.tsx
@@ -1,13 +1,6 @@
-import { redirect } from 'next/navigation';
-import { auth } from '@/auth';
 import NewBlogForm from './NewBlogForm';
 
-const NewBlogPage = async () => {
-  const session = await auth();
-  if (!session) {
-    redirect('/login');
-  }
-
+const NewBlogPage = () => {
   return <NewBlogForm />;
 };
 
